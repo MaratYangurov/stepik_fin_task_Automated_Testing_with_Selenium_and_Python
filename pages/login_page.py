73d4185c9b323d50b114384de9d7285f9bb66ebb
MaratYangurov <marat.yangurov@gmail.com>
2026-06-18 17:35:18 +0500

    Коммит_5: Реализация LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,14 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        # Проверяет, что в URL присутствует подстрока "login"
+        current_url = self.browser.current_url
+        assert "login" in current_url, f'Expected substring "login" in URL, but received "{current_url}"'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_form"), 'Login form is not presented on the page'
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(By.CSS_SELECTOR, "#register_form"), 'Register form is not presented on the page'
