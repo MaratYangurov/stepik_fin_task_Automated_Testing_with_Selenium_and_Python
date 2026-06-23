461417bb1e2af998ff64ab64e23310de83d88d0d
MaratYangurov <marat.yangurov@gmail.com>
2026-06-23 18:31:03 +0500

    Коммит_8: Задание: независимость контента, ищем баг

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,36 +6,53 @@ from selenium.common.exceptions import NoAlertPresentException
 
 class ProductPage(BasePage):
     ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
 
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
-    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success .alertinner strong")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
 
     def add_product_to_basket(self):
         button = self.browser.find_element(*self.ADD_TO_BASKET)
         button.click()
 
-    def should_be_product_name_in_message(self):
-        name = self.browser.find_element(*self.PRODUCT_NAME).text
-        message = self.browser.find_element(*self.SUCCESS_MESSAGE).text
-        assert name == message, f"Expected {name}, got {message}"
+    def get_product_name(self):
+        # Возвращает название товара со страницы
+        return self.browser.find_element(*self.PRODUCT_NAME).text
 
-    def should_be_price_in_basket(self):
-        price = self.browser.find_element(*self.PRODUCT_PRICE).text
-        basket_price = self.browser.find_element(*self.BASKET_PRICE).text
-        assert price == basket_price, f"Expected {price}, got {basket_price}"
+    def get_product_price(self):
+        # Возвращает цену товара со страницы
+        return self.browser.find_element(*self.PRODUCT_PRICE).text
+
+    def should_be_product_name_in_message(self, expected_name):
+        # Проверяем название товара в сообщении
+        actual_name = self.browser.find_element(
+            *self.SUCCESS_MESSAGE
+        ).text
+        assert actual_name == expected_name, \
+            f"Expected '{expected_name}', got '{actual_name}'"
+
+    def should_be_price_in_basket(self, expected_price):
+        # Проверяем цену корзины
+        actual_price = self.browser.find_element(
+            *self.BASKET_PRICE
+        ).text
+
+        assert actual_price == expected_price, \
+            f"Expected '{expected_price}', got '{actual_price}'"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        answer = str(
+            math.log(abs(12 * math.sin(float(x))))
+        )
         alert.send_keys(answer)
         alert.accept()
         try:
             alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
+            print(f"Your code: {alert.text}")
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
--- test_product_page.py
+++ test_product_page.py
@@ -5,22 +5,51 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
-from pages.product_page import ProductPage
+
 
 
 class TestAddToCart:
-    def test_guest_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    @pytest.mark.parametrize(
+        'link',
+        [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                     marks=pytest.mark.xfail),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+        ])
+    def test_guest_can_add_product_to_basket(self, browser, link):
+        browser.get(link)
 
-        page = ProductPage(browser, link)
-        page.open()
+        # добавляем товар
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        button.click()
 
-        page.add_product_to_basket()
-        page.solve_quiz_and_get_code()
+        # решаем
+        alert = browser.switch_to.alert
+        x = alert.text.split()[2]
+        answer = str(math.log(abs(12 * math.sin(float(x)))))
+        alert.send_keys(answer)
+        alert.accept()
 
-        page.should_be_product_name_in_message()
-        page.should_be_price_in_basket()
+        # второй alert с кодом
+        try:
+            alert = browser.switch_to.alert
+            alert.accept()
+        except NoAlertPresentException:
+            pass
 
+        # проверки
+        product_name = browser.find_element(By.CSS_SELECTOR, ".product_main h1").text
+        success_message = browser.find_element(By.CSS_SELECTOR, ".alertinner strong").text
+
+        assert product_name == success_message, f"Bug on link: {link}"
 
 
 # pytest -v --tb=line --language=en test_product_page.py
