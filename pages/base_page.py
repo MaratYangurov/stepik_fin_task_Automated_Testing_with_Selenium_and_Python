9b75e9fda47d449ff248b9b3c01fe604f9a27549
Marat_Yangurov <marat.yangurov@gmail.com>
2026-06-28 17:11:05 +0500

    Коммит_10: Плюсы наследования: пример

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -37,3 +38,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
+from .pages.product_page import ProductPage
 
 
 
@@ -50,6 +51,30 @@ class TestAddToCart:
         success_message = browser.find_element(By.CSS_SELECTOR, ".alertinner strong").text
 
         assert product_name == success_message, f"Bug on link: {link}"
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    # test_product_page.py
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        """
+        Тест: гость может перейти на страницу логина со страницы товара
+        """
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+        page = ProductPage(browser, link)
+        page.open()
+
+        # Переходим на страницу логина (метод из BasePage)
+        page.go_to_login_page()
+
+        # Проверяем, что мы действительно на странице логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
 # pytest -v --tb=line --language=en test_product_page.py
